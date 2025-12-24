5911984e58a5339960bd7be9b332911225c22434
w2user01 <w2user01@rpsconsulting.in>
2025-12-24 17:06:57 +0530

    Day 4 Notes and class room examples
    Day 4 Notes and examples shared

---
+++ dayFour/dataTypesOne.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main(){
+	int varOne = 100;
+	auto varTwo = 200;
+
+	cout<<"varOne: "<<varOne<<"\tvarTwo: "<<varTwo<<endl;
+}
+
---
+++ dayFour/globalScope.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int var = 100;
+
+int main(){
+	int var = 200;
+	cout<<"var: "<<var<<"\t::var: "<<::var<<endl;
+}
+
+
